26202699bb25c7c4a700ee1b28fcdafca7b0ded2
vermei2@outlook.com <vermei2@outlook.com>
2022-01-23 20:37:50 +0300

    Добавьте файлы проекта.

---
+++ Lab14/Cat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Cat : Animal
+    {
+     
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Cat(string name)
+            : base(name)
+        { }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
---
+++ Lab14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             {
+                Dog dog = new Dog ("Шарик");
+                dog.ShowInfo();
+                Cat cat = new Cat("Маруся");
+                cat.ShowInfo();
+                Console.ReadKey();
+             }
+        }
+    }
+}
